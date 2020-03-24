77db8f3a65ef71c2dbafac9aa98710c7a7582bf8
klhaim7 <klhaim7@gmail.com>
2020-03-24 23:18:42 +0200

    commit

---
+++ src/test/java/com/bitnology/microservices/netflixzuulapigatwayserver/NetflixZuulApiGatwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitnology.microservices.netflixzuulapigatwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatwayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
